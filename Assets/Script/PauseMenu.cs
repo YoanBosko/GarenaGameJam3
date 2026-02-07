33531d08a02d50d76dbe88400415394d6b4a4af1
YoanBosko <yoanbosko@gmail.com>
2026-02-07 23:07:13 +0700

    maus

--- Assets/Script/PauseMenu.cs
+++ Assets/Script/PauseMenu.cs
@@ -1,16 +1,73 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseUI;
+    // Struktur untuk menyimpan data transformasi awal
+    private struct InitialTransform
+    {
+        public Transform transform;
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 scale;
+    }
+
+    private List<InitialTransform> childData = new List<InitialTransform>();
+
+    void Start()
+    {
+        // Simpan posisi awal semua child dari pauseUI saat pertama kali dijalankan
+        if (pauseUI != null)
+        {
+            foreach (Transform child in pauseUI.transform)
+            {
+                InitialTransform data = new InitialTransform
+                {
+                    transform = child,
+                    position = child.localPosition,
+                    rotation = child.localRotation,
+                    scale = child.localScale
+                };
+                childData.Add(data);
+            }
+        }
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            GameManager.Instance.TogglePause();
-            pauseUI.SetActive(GameManager.Instance.IsPaused);
+            TogglePauseMenu();
+        }
+    }
+
+    private void TogglePauseMenu()
+    {
+        GameManager.Instance.TogglePause();
+        bool isPaused = GameManager.Instance.IsPaused;
+
+        if (isPaused)
+        {
+            // Reset posisi semua child sebelum menampilkan UI
+            ResetChildPositions();
+        }
+
+        pauseUI.SetActive(isPaused);
+    }
+
+    // Fungsi untuk mengembalikan posisi semua child ke tatanan semula
+    private void ResetChildPositions()
+    {
+        foreach (var data in childData)
+        {
+            if (data.transform != null)
+            {
+                data.transform.localPosition = data.position;
+                data.transform.localRotation = data.rotation;
+                data.transform.localScale = data.scale;
+            }
         }
     }
 
